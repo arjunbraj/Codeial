8313143a620cf0c3238dfc2e1b48be06f8d2000b
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-01-29 21:23:01 +0530

    added styles to header and footer

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 
 // setting up sass
-const sassMiddleware = require('node-sass');
-app.use(sassMiddleware({
-    src: '/assets/scss',
-    dest: '/assets/css',
-    debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+// const sassMiddleware = require('node-sass-middleware');
+// app.use(sassMiddleware({
+//     src: '/assets/scss',
+//     dest: '/assets/css',
+//     debug: true,
+//     outputStyle: 'extended',
+//     prefix: '/css'
+// }));
 
 // add cookie parser
 app.use(cookieParser());

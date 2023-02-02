642b9d43f5962dde3300f2e1ce854fa0acb38de4
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-02-02 17:59:49 +0530

    created links for friend list

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,8 +2,11 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('profile', {
-        title: 'Profile'
+    User.findById(req.params.id, function(err, user){
+        return res.render('profile', {
+            title: 'Profile',
+            profile_user: user
+        });
     });
 }
 

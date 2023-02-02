e20b6470f033c90719b74449964561e711a25434
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-02-02 20:24:37 +0530

    added async await to controllers

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,34 +1,39 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+    try {
+        let post = await Post.findById(req.body.post);
         if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
-                if(err){console.log('Error occurred in creating comment');}
-                post.comments.push(comment);
-                post.save();
-                res.redirect('/');
-            });
+        let comment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
+        post.comments.push(comment);
+        post.save();
+        res.redirect('/');
         }
-    });
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 }
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+module.exports.destroy = async function(req, res){
+    try{
+        let comment = await Comment.findById(req.params.id);
         if(comment.user == req.user.id){
             let postId = comment.post;
             comment.remove();
 
-            Post.findById(postId, {$pull: {comments: req.params.id}}, function(err, post){
-                return res.redirect('back');
-            });
+            await Post.findById(postId, {$pull: {comments: req.params.id}});
+            return res.redirect('back');
         } else{
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log(err);
+        return;
+    }
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -47,28 +47,23 @@ module.exports.signIn = function(req, res){
 }
 
 // get data from sign up
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
     // confirm password
-    if(req.body.password != req.body.confirm_password){
-        return res.redirect('back');
-    }
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log('Error in finding user for signing up'); 
-            return;
+    try{
+        if(req.body.password != req.body.confirm_password){
+            return res.redirect('back');
         }
+        let user = await User.findOne({email: req.body.email});
         if(!user){
-            User.create(req.body, function(err, user){
-                if(err){
-                    console.log('Error in creating user for signing up'); 
-                    return;
-                }
-                return res.redirect('/users/sign-in');
-            });
+            await User.create(req.body);
+            return res.redirect('/users/sign-in');
         }else{
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log(err);
+        return;
+    }
 }
 
 // sign in and create session for user

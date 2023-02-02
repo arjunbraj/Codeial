3dd21411fb0e9aaa6ea571f12477230cc9362ee1
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-02-02 15:39:44 +0530

    added comment section

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.get('/', homeController.home);
 router.get('/about', homeController.about);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 module.exports = router;
